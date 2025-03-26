21c376f546cdfc965ac40a477f1b790cd0339d19
camila.moreira <camilammoreira1@gmail.com>
2025-03-26 11:04:36 -0300

    send new chats

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,19 @@
 const chatList = document.querySelector("ul");
+const chatForm = document.querySelector(".chat-form");
+const nameForm = document.querySelector(".name-form");
 
 // class instances
 const chatUI = new ChatUI(chatList);
 const chatroom = new Chatroom("general", "camila");
+
+// user add message
+chatForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const newChat = chatForm.message.value.trim();
+    chatroom.addChat(newChat)
+        .then(() => chatForm.reset())
+        .catch(err => console.log(err));
+});
 
 // get chats and render
 chatroom.getChats(data => chatUI.render(data));
