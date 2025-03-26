cce02cfc4d5ecbd56aa6548babf32057e00cb740
camila.moreira <camilammoreira1@gmail.com>
2025-03-26 10:23:46 -0300

    format dates

--- scripts/ui.js
+++ scripts/ui.js
@@ -6,11 +6,18 @@ class ChatUI {
     }
     // render chat templates to the DOM
     render(chat) {
-        let html = `
+        const when = dateFns.formatDistanceToNow(
+            chat.created_at.toDate(),
+            {
+                addSuffix: true,
+                includeSeconds: true
+            }
+        );
+        const html = `
             <li class="list-group-item">
                 <span><strong>${chat.username}</strong></span>
                 <span>${chat.message}</span>
-                <p class="text-muted"><small>${chat.created_at.toDate()}</small></p>
+                <p class="text-muted"><small>${when}</small></p>
             </li>
         `;
         this.list.innerHTML += html;
