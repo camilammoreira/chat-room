e5a7fbea5959dbc0500b810928741688138171f8
camila.moreira <camilammoreira1@gmail.com>
2025-03-26 09:22:41 -0300

    update room & username

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,11 @@
-// adding new chat documents
-// setting up a real-time listener to get new chats
-// updating the username
-// updating the room
-
 class Chatroom {
     constructor(room, username) {
         this.room = room;
         this.username = username;
         this.chats = db.collection("chats");
+        this.unsub;
     }
+    // adding new chat documents
     async addChat(message) {
         // format chat object
         const now = new Date();
@@ -21,10 +18,46 @@ class Chatroom {
         const response = await this.chats.add(chat);
         return this;
     }
+    // setting up a real-time listener to get new chats
+    getChats(callback) {
+        this.unsub = this.chats
+            .where("room", "==", this.room)
+            .orderBy("created_at")
+            .onSnapshot(snapshot => {
+                snapshot.docChanges().forEach(change => {
+
+                    if (change.type === "added") {
+                        //update the ui
+                        callback(change.doc.data());
+                    }
+                })
+            })
+    }
+    // updating the username
+    updateName(username) {
+        this.username = username;
+    }
+    // updating the room
+    updateRoom(room) {
+        this.room = room;
+        console.log("room updated");
+        if (this.unsub) {
+            this.unsub();
+        }
+    }
+
 }
 
-// const general = new Chatroom("general", "camila");
+const chatroom = new Chatroom("general", "camila");
 
-// general.addChat("hey there")
-//     .then(() => console.log("chat added"))
-//     .catch(err => console.log(err));
+chatroom.addChat("hi general");
+chatroom.getChats(data => console.log(data))
+
+
+// user simulation
+setTimeout(() => {
+    chatroom.updateRoom("music");
+    chatroom.updateName("vanda");
+    chatroom.getChats(data => console.log(data));
+    chatroom.addChat("listen to chapel roan");
+}, 3000)
